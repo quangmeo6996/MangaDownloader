66e34f83dc665de43323014409ae674194b0915e
quangmeo6996 <152080287+quangmeo6996@users.noreply.github.com>
2026-01-18 18:06:36 -0500

    .

--- build.bat
+++
@@ -1,7 +0,0 @@
-jpackage ^
-  --type app-image ^
-  --input target ^
-  --main-jar MangaDownloader-1.0-SNAPSHOT-jar-with-dependencies.jar ^
-  --name mangadownloader ^
-  --dest dist ^
-  --win-console
